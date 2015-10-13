ae5399f97ed30125c7e1b15cb2a6138d723cb4db
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-13 14:53:42 +1100

    Bug1Test class

---
+++ CrownAndAnchorGame/src/BugOneTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class BugOneTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
