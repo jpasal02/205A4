8cf0ee8ebc2eaea30b9326b80ad3ae6de6944858
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-13 22:25:47 +1100

    Bug2 test class created

---
+++ CrownAndAnchorGame/src/BugTwoTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class BugTwoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
