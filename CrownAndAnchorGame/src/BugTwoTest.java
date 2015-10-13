6adb2964f918845af950ea055d139ce7b3cf4812
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-13 22:28:33 +1100

    Setup bug 2

--- CrownAndAnchorGame/src/BugTwoTest.java
+++ CrownAndAnchorGame/src/BugTwoTest.java
@@ -6,9 +6,13 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class BugTwoTest {
+	
+	private static Player player_;
 
 	@Before
 	public void setUp() throws Exception {
+		player_ = new Player("Robbert", 20);
+		player_.setLimit(0);
 	}
 
 	@Test
