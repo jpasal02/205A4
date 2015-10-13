75d2c2b969656499db7ab7d87bb9aabdad9c9bff
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-13 22:56:15 +1100

    TEST bug2 complete YAAAAY

--- CrownAndAnchorGame/src/BugTwoTest.java
+++ CrownAndAnchorGame/src/BugTwoTest.java
@@ -16,8 +16,42 @@ public class BugTwoTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGameStopsAtLimit() {
+		int bet = 5;
+		
+		player_ = new Player("Joe", 5); //Does not exceed limit 
+		System.out.println("player: " + player_.getName());
+		System.out.println("Balance: " + player_.getBalance());
+		System.out.println("Balance < limit " + player_.balanceExceedsLimitBy(bet));
+		
+		assertEquals(player_.balanceExceedsLimitBy(bet), true);
+		
+		player_ = new Player("Joe", 10); //Does not exceed limit
+		System.out.println("player: " + player_.getName());
+		System.out.println("Balance: " + player_.getBalance());
+		System.out.println("Balance < limit " + player_.balanceExceedsLimitBy(bet));
+		equals(player_.balanceExceedsLimitBy(bet));
+		
+		assertEquals(player_.balanceExceedsLimitBy(bet), true);
+		
+		player_ = new Player("Joe", 15); //Does not exceed limit 
+		System.out.println("player: " + player_.getName());
+		System.out.println("Balance: " + player_.getBalance());
+		System.out.println("Balance < limit " + player_.balanceExceedsLimitBy(bet));
+		equals(player_.balanceExceedsLimitBy(bet));
+		
+		assertEquals(player_.balanceExceedsLimitBy(bet), true);
+		
+		player_ = new Player("Joe", 0);  // exceeds limit 
+		System.out.println("player: " + player_.getName());
+		System.out.println("Balance: " + player_.getBalance());
+		System.out.println("Balance < limit " + player_.balanceExceedsLimitBy(bet));
+		
+		assertEquals(player_.balanceExceedsLimitBy(bet), false);
+		
+		
+		
+		//fail("Not yet implemented");
 	}
 
 }
