d29f4ebfca420d72d3315ea4523ab8eab16b3f8e
jpasal02 <jasmina-pasalic@hotmail.com>
2015-10-14 15:08:25 +1100

    testbug 3

--- CrownAndAnchorGame/src/BugThreeTest.java
+++ CrownAndAnchorGame/src/BugThreeTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,29 +29,117 @@ public class BugThreeTest {
 	}
 
 	@Test
-	public void test() {
+	public void testRatioWinLose() {
+//		int bet = 5;
+//		int winCount = 0; 
+//		int loseCount = 0;
+//		//DiceValue pick = DiceValue.getRandom();
+//
+//		when(mockD1_.getValue()).thenReturn(DiceValue.getRandom());
+//		when(mockD2_.getValue()).thenReturn(DiceValue.getRandom());
+//		when(mockD3_.getValue()).thenReturn(DiceValue.getRandom());
+//		
+//		
+//		DiceValue pick = DiceValue.getRandom();
+//		game_ = new Game(mockD1_, mockD2_, mockD3_);
+//		
+//		player_.takeBet(5);
+//		
+//		int winnings_ = game_.playRound(player_, pick, bet);
+//		
+//		if (winnings_ > 0){
+//			winCount++;
+//		}
+//		else
+//		{
+//			loseCount++;
+//		}
+//		
+//		int gamesPlayed = 100;
+//		
+//		float ratio = (winCount * 100) / (winCount + loseCount);
+//		System.out.println("WINS:	" + winCount +" LOSE: " +loseCount + "	 RATIO:		"+ ratio);
+//		
+//		assertTrue(ratio > 0.30 && ratio < 0.50);
+//		
+//	}
+//
+//}
+
+		Player player = new Player("Fred", 100);
+		player.setLimit(0);
 		int bet = 5;
-		int winCount = 1; 
-		int loseCount = 1;
-		DiceValue pick = DiceValue.getRandom();
-		game_ = new Game(mockD1_, mockD2_, mockD3_);
+		player.takeBet(bet);
 		
-		player_.takeBet(5);
-		
-		int winnings_ = game_.playRound(player_, pick, bet);
-		
-		if (winnings_ > 0){
-			winCount++;
-		}
-		else
-		{
-			loseCount++;
-		}
-		
-		float ratio = winCount/(winCount + loseCount);
-		System.out.println(winCount + loseCount + ratio);
-		assertTrue(ratio > 0.30 && ratio < 0.50);
+		when(mockD1_.getValue()).thenReturn(DiceValue.getRandom());
+		when(mockD2_.getValue()).thenReturn(DiceValue.getRandom());
+		when(mockD3_.getValue()).thenReturn(DiceValue.getRandom());
+       
+        //List<DiceValue> cdv = game_.getDiceValues();
+        game_ = new Game(mockD1_, mockD2_, mockD3_);
+        List<DiceValue> cdv = game_.getDiceValues();
+        
+       
+
+        int totalWins = 0;
+        int totalLosses = 0;
+
+        while (true)
+        {
+            int winCount = 0;
+            int loseCount = 0;
+            
+            for (int i = 0; i < 100; i++)
+            {
+
+                System.out.println(String.format("Start Game %d: ", i));
+                System.out.println(String.format("%s starts with balance %d, limit %d", 
+                		player.getName(), player.getBalance(), player.getLimit()));
+
+                int turn = 0;
+                while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
+                {
+                    turn++;                    
+                	DiceValue pick = DiceValue.getRandom();
+                   
+                	System.out.printf("Turn %d: %s bet %d on %s\n",
+                			turn, player.getName(), bet, pick); 
+                	
+                	int winnings = game_.playRound(player, pick, bet);
+                	cdv = game_.getDiceValues();
+                    
+                    System.out.printf("Rolled %s, %s, %s\n",
+                    		cdv.get(0), cdv.get(1), cdv.get(2));
+        
+                    
+                    if (winnings > 0) {
+	                    System.out.printf("%s won %d, balance now %d\n\n",
+	                    		player.getName(), winnings, player.getBalance());
+	                	winCount++; 
+                    }
+                    else {
+	                    System.out.printf("%s lost, balance now %d\n\n",
+	                    		player.getName(), player.getBalance());
+	                	loseCount++;
+                    }
+                    
+                } //while
+
+                System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
+                System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
+                
+            } //for
+            
+            System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
+            totalWins += winCount;
+            totalLosses += loseCount;
+            
+            float ratio = (totalWins * 100) / (totalWins + totalLosses);
+            System.out.println("WINS:	" + winCount +" LOSE: " +loseCount + "	 RATIO:		"+ ratio);
+            assertTrue(ratio > 0.30 && ratio < 0.50);
 		
 	}
-
+	
+	}
+	
 }
